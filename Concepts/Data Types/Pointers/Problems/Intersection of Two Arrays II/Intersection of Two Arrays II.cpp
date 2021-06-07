d5d7d46a2571fd4499f7ec0c2ee3185d5f88643c
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-06-07 19:01:17 +0530

    Uploaded Intersection of Two Arrays II problem and algorithm.

---
+++ Concepts/Data Types/Pointers/Problems/Intersection of Two Arrays II/Intersection of Two Arrays II.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    sort(nums1.begin(),nums1.end());
+    sort(nums2.begin(),nums2.end());
+    vector<int> v;
+    int i=0,j=0;
+    while(i<nums1.size() && j<nums2.size()){
+        if(nums1[i] == nums2[j]){
+            v.push_back(nums1[i]);
+            i++;j++;
+        }else if(nums1[i] > nums2[j]) j++;
+        else i++;
+    }
+    return v;
+}
+
+int main() {
+	vector<int> nums1 = {4,9,4,5};
+	vector<int> nums2 = {9,4,9,8,4};
+	nums1 = intersection(nums1,nums2);
+	for(int i=0;i<nums1.size();i++) cout<<nums1[i]<<" ";
+	return 0;
+}
