7768e02625e5984ac4195c883b2139984419bc44
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-06-15 22:56:36 +0530

    Modified the mistake in Code.

--- Problems/176 Swap Consecutive Index Pairs/Swap Consecutive Index Pairs.cpp
+++ Problems/176 Swap Consecutive Index Pairs/Swap Consecutive Index Pairs.cpp
@@ -1,12 +1,12 @@
 vector<int> solve(vector<int>& nums) {
     vector<int> v;
     int l = nums.size();
-    for(int i=0;i<l-1;i+=4){
+    for(int i=0;i<l-2;i+=4){
         int t = nums[i];
         nums[i] = nums[i+2];
         nums[i+2] = t;
     }
-    for(int i=1;i<l-1;i+=4){
+    for(int i=1;i<l-2;i+=4){
         int t = nums[i];
         nums[i] = nums[i+2];
         nums[i+2] = t;
