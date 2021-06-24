9438bdd64ef85f495aa743a4fbea063ffb46a1ff
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-06-24 23:18:26 +0530

    Uploaded Compute the integer absolute value (abs) without branching problem and algorithm.

---
+++ Problems/220 Compute the integer absolute value (abs) without branching()()()/Compute the integer absolute value (abs) without branching.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define CHARBIT 8
+
+
+unsigned int getAbs(int n)
+{
+    int const mask = n >> (sizeof(int) * CHAR_BIT - 1);
+    return ((n ^ mask) - mask);
+}
+
+int main()
+{
+	int n = -6;
+	cout << "Absoute value of " << n << " is " << getAbs(n);
+	return 0;
+}
