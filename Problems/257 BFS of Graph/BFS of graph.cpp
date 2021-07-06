ba8ba36b66573a191fe7a1e3a59261cc0e2fa006
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-07-06 22:44:42 +0530

    Uploaded BFS of graph problem and algorithm.

---
+++ Problems/257 BFS of Graph/BFS of graph.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+	vector<int>bfsOfGraph(int V, vector<int> adj[]){
+	    vector<int> vec;
+	    vector<int> check(V,0);
+	    queue<int> q;
+	    int a;
+	    q.push(0);
+	    check[0] = 1;
+	    while(!q.empty()){
+	        a = q.front();
+	        q.pop();
+	        vec.push_back(a);
+	        for(int i=0;i<adj[a].size();i++){
+	            if(check[adj[a][i]] == 0){
+	                q.push(adj[a][i]);
+	                check[adj[a][i]] = 1;
+	            }
+	        }
+	    }
+	    return vec;
+	}
+};
+
+int main(){
+	int V, E;
+	cin >> V >> E;
+	vector<int> adj[V];
+	for(int i = 0; i < E; i++){
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+	}
+    Solution obj;
+    vector<int>ans=obj.bfsOfGraph(V, adj);
+    for(int i=0;i<ans.size();i++){
+    	cout<<ans[i]<<" ";
+    }
+	return 0;
+}
+
+//Input:
+//10 26
+//0 2
+//0 3
+//0 4
+//0 5
+//0 6
+//0 7
+//1 2
+//1 3
+//1 4
+//1 5
+//1 7
+//1 8
+//2 3
+//2 5
+//2 7
+//2 8
+//2 9
+//3 4
+//3 7
+//3 8
+//3 9
+//4 5
+//5 7
+//5 9
+//6 8
+//8 9
+//Output:
+//0 2 3 4 5 6 7 8 9
