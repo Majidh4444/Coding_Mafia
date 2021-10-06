014e4b4579ce9012b975229a4440a4acbf3e406e
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-10-06 23:41:32 +0530

    Uploaded cpp file which contains important functions to character manipulations.

---
+++ Concepts/Basics/characterManipulations.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void toCheckAlpha(char ch){
+    //*******************************************************************************************************************//
+    if(isalpha(ch)) cout<<"YES => isalpha()"<<endl;
+}
+
+void toCheckDigit(char ch){
+    //*******************************************************************************************************************//
+    if(isdigit(ch)) cout<<"YES => isdigit()"<<endl;
+}
+
+void toCheckAlphaNumerical(char ch){
+    //*******************************************************************************************************************//
+    if(isalnum(ch)) cout<<"YES => isalnum()"<<endl;
+}
+
+void changeToLower(char ch){
+    //*******************************************************************************************************************//
+    cout<<char(tolower(ch))<<" => tolower() which return ASCII value"<<endl;
+}
+
+void changeToUpper(char ch){
+    //*******************************************************************************************************************//
+    cout<<char(toupper(ch))<<" => toupper() which return ASCII value we have to change it."<<endl;
+}
+
+
+int main(){
+	char ch = 'A';
+
+    toCheckAlphaNumerical(ch);
+
+    toCheckAlpha(ch);
+
+    toCheckDigit(ch);
+
+    changeToLower(ch);
+
+	return 0;
+}
