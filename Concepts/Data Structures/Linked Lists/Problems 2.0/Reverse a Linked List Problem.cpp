c5b500bcd3c78e1c7fed36d9392b67e15a9f9007
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-11-17 23:08:30 +0530

    Uploaded Reverse a Linked List problem's algorithm.

---
+++ Concepts/Data Structures/Linked Lists/Problems 2.0/Reverse a Linked List Problem.cpp
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+    
+}*start;
+
+void insert();
+void display(struct Node *head);
+
+struct Node* reverseList(struct Node *head){
+    struct Node* prev = NULL;
+    struct Node* current = head;
+    struct Node* nxt = head->next;
+    
+    
+    while(current){
+        nxt = current->next;
+        current->next = prev;
+        prev = current;
+        current = nxt;
+        if(!current) break;
+        nxt = nxt->next;
+    }
+    
+    return prev;
+}
+
+int main(){
+    start=NULL;
+    insert();
+    start = reverseList(start);
+    display(start);
+    printf("\n");
+    return 0;
+
+}
+
+
+void insert(){
+     int n,value,i;
+     scanf("%d",&n);
+     struct Node *temp;
+     for(i=0;i<n;i++){
+         scanf("%d",&value);
+         if(i==0){
+              start=(struct Node *) malloc( sizeof(struct Node) );
+              start->data=value;
+              start->next=NULL;
+              temp=start;
+              continue;
+         }else{
+             temp->next= (struct Node *) malloc( sizeof(struct Node) );
+             temp=temp->next;
+             temp->data=value;
+             temp->next=NULL;
+         }
+     }
+ }
+ 
+void display(struct Node *head){
+   while(head!=NULL){
+   printf("%d ",head->data);
+    head=head->next;
+  }
+}
