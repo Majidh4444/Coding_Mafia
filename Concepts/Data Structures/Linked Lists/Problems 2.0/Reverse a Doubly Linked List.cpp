6bb2bf00414153426ac7fd13b9a99f5f12d69dd9
Fahadh Kasala <mdfahadh2000@gmail.com>
2022-01-09 23:47:56 +0530

    Uploaded Reverse a Doubly Linked List algorithm.

---
+++ Concepts/Data Structures/Linked Lists/Problems 2.0/Reverse a Doubly Linked List.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x){
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+
+Node *newNode(int data){
+    Node *temp=new Node(data);
+    return temp;
+}
+
+
+
+
+void displayList(Node *head){
+    while(head->next){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<head->data;
+}
+
+
+int getLength(Node * head){
+    Node *temp=head;
+    
+    int count=0;
+    while(temp->next!=head){
+        count++;
+        temp=temp->next;
+    }
+    return count+1;
+}
+
+
+
+
+bool verify(Node* head){
+    int fl=0;
+    int bl=0;
+    
+    Node *temp=head;
+    
+    while(temp->next){
+        temp=temp->next;
+        fl++;
+    }
+    
+    while(temp->prev){
+        temp=temp->prev;
+        bl++;
+    }
+    
+    return fl==bl;
+}
+
+
+Node* reverseDLL(Node * head){
+    Node* temp;
+    while(head->next){
+        temp = head->next;
+        head->next = head->prev;
+        head->prev = temp;
+        head = temp;
+    }
+    
+    temp = head->next;
+    head->next = head->prev;
+    head->prev = temp;
+    
+    return head;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    Node *head=NULL, *tail=NULL;
+    int x;
+    cin>>x;
+    head = newNode(x);
+    tail = head;
+    
+    for(int i=0;i<n - 1;i++){
+        cin>>x;
+        Node* temp=newNode(x);
+        tail->next=temp;
+        temp->prev= tail;
+        tail = temp;
+    }
+    head=reverseDLL(head);
+    
+    
+    if(verify(head))
+    displayList(head);
+    else
+    cout<<"Your pointers are not correctly connected";
+	return 0;
+}
