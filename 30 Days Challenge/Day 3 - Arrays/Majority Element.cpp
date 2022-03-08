6e90ccb85254900ae3f6f3c7357bf799dd37badb
Fahadh Kasala <mdfahadh2000@gmail.com>
2022-03-08 23:10:13 +0530

    Uploading 30 Days Challenge Day 03 Problem 03 - Majority Element algorithm.

---
+++ 30 Days Challenge/Day 3 - Arrays/Majority Element.cpp
@@ -0,0 +1,19 @@
+// Use Boyer–Moore Majority Vote Algorithm(Searching Algorithm)
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int temp = 0;
+        for(int i=0;i<nums.size();i++){
+            if(count == 0){
+                temp = nums[i];
+                count++;
+            }else{
+                if(nums[i] == temp) count++;
+                else count--;
+            }
+        }
+        return temp;
+    }
+};
